8b17887c0e6e8ac8b9bf76981c8d41e09aa87f83
Bhumika-Raut <bhumikaraut59620@gmail.com>
2024-01-23 17:51:15 +0530

    calculator

---
+++ calculator/src/App.jsx
@@ -0,0 +1,18 @@
+// import React from 'react'
+import { useState } from 'react'
+import Key from './components/Keys'
+import './App.css'
+
+function App() {
+  // const [name, setName] = useState("");
+
+  return (
+
+    <div className='App'>
+      <h1>Calculate</h1>
+      <Key />
+    </div>
+  )
+}
+
+export default App
